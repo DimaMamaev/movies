d4f7049453742b93e958430ec295b6e8f8cababc
DimaMamaev <dmamaev@my.com>
2020-07-13 20:59:19 +0300

    connected received data for grid component & created lazy image loading functionality

--- src/components/content/grid/Grid.js
+++ src/components/content/grid/Grid.js
@@ -1,35 +1,53 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import "./grid.scss";
 import Rating from "../rating/Rating";
+import { IMAGE_URL } from "../../../services/movies.service";
+import { v4 as uuidv4 } from "uuid";
+import LazyImage from "../../lazyImage/lazyImage";
 
-const Grid = ({ images }) => {
+const Grid = (props) => {
+  const { list } = props;
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    setMovieData(list);
+  }, [list]);
   return (
     <>
       <div className="grid">
-        {images.map((image, i) => (
-          <div key={i}>
-            <div
+        {movieData.map((data) => (
+          <div key={uuidv4()}>
+            <LazyImage
               className="grid-cell"
-              style={{ backgroundImage: `url(${image.url})` }}
+              src={`${IMAGE_URL}${data.poster_path}`}
+              alt="placeholder"
             >
               <div className="grid-read-more">
                 <button className="grid-cell-button">Read More</button>
               </div>
               <div className="grid-detail">
-                <span className="grid-detail-title">Mission Impossible</span>
+                <span className="grid-detail-title">{data.title}</span>
                 <div className="grid-detail-rating">
-                  <Rating rating={image.rating} totalStars={10} />
+                  <Rating rating={data.vote_average} totalStars={10} />
                   &nbsp;&nbsp;
-                  <div className="grid-vote-average">{image.rating}</div>
+                  <div className="grid-vote-average">{data.vote_average}</div>
                 </div>
               </div>
-            </div>
+            </LazyImage>
           </div>
         ))}
       </div>
     </>
   );
 };
+Grid.propTypes = {
+  list: PropTypes.array.isRequired,
+};
 
-export default Grid;
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+});
+
+export default connect(mapStateToProps, {})(Grid);
