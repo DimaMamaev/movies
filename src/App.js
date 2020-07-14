5a678ef6b4a409834ae0cb1e4b1c37460f8c746b
DimaMamaev <dmamaev@my.com>
2020-07-14 22:14:40 +0300

    created error page mock

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/main/Main";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Details from "./components/content/details/Details";
 import "./app.scss";
+import ErrorPage from "./components/error/ErrorPage";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Main} />
           <Route exact path="/movies" component={Main} />
           <Route exact path="/:id/:name/details" component={Details} />
+          <Route path="*" component={ErrorPage} />
         </Switch>
       </div>
     </BrowserRouter>
